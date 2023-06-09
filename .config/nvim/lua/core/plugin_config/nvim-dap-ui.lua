3ef0519a632149062e5a978ebca1492cb959a9e1
Hasan Gökhan Açıkgöz <gokhan.acikgoz@deliveryhero.com>
2023-06-09 12:42:08 +0300

    add which key and dap ui confs

---
+++ .config/nvim/lua/core/plugin_config/nvim-dap-ui.lua
@@ -0,0 +1,44 @@
+require("dapui").setup()
+
+
+local dap, dapui =require("dap"),require("dapui")
+dap.listeners.after.event_initialized["dapui_config"]=function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"]=function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"]=function()
+  dapui.close()
+end
+
+
+-- Start debugging session
+vim.keymap.set("n", "<localleader>ds", function()
+  dap.continue()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
+end)
+ 
+-- Set breakpoints, get variable values, step into/out of functions, etc.
+vim.keymap.set("n", "<localleader>dl", require("dap.ui.widgets").hover)
+vim.keymap.set("n", "<localleader>dc", dap.continue)
+vim.keymap.set("n", "<localleader>db", dap.toggle_breakpoint)
+vim.keymap.set("n", "<localleader>dn", dap.step_over)
+vim.keymap.set("n", "<localleader>di", dap.step_into)
+vim.keymap.set("n", "<localleader>do", dap.step_out)
+vim.keymap.set("n", "<localleader>dC", function()
+  dap.clear_breakpoints()
+  require("notify")("Breakpoints cleared", "warn")
+end)
+ 
+-- Close debugger and clear breakpoints
+vim.keymap.set("n", "<localleader>de", function()
+  dap.clear_breakpoints()
+  dap.terminate()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+  require("notify")("Debugger session ended", "warn")
+end)
+
+
+vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
